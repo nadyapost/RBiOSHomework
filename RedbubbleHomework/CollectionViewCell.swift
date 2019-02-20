ae1f546d418f9bdaaf4867d1d468f816c9597f56
nadyapost <nadyapostr@gmail.com>
2019-02-20 11:29:34 +1100

    Creat IUScrollView and  add datasource methods.

---
+++ RedbubbleHomework/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  RedbubbleHomework
+//
+//  Created by Nadya Postriganova on 20/2/19.
+//  Copyright © 2019 Redbubble. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var artworksImageView: UIImageView!
+    
+}
--- RedbubbleHomework/ViewController.swift
+++ RedbubbleHomework/ViewController.swift
@@ -5,16 +5,42 @@
 import UIKit
 import RealmSwift
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let artworkImages: [UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
     }
 
+    
+    
+    // Set Datasource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return artworkImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+        
+        cell.artworksImageView.image = artworkImages[indexPath.item]
+        
+        return cell
+    }
+    
+    // Set Delegates
+    
+    
+    
 }
